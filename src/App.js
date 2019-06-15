fb7764ae9d012cf7d6170aa1e6067213a3729786
William Hill <mjh2225@gmail.com>
2019-06-14 20:31:05 -0700

    added initial buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  ButtonGroup,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+const scalarOptions = [
+  "Spatial Distribution Score",
+  "Bias Score",
+  "Overall Score",
+  "Seasonal Cycle Score",
+  "Interannual Variability Score",
+  "RMSE Score",
+  "Amplitude Score",
+  "Max Phase Score",
+  "Min Phase Score",
+  "Difference Score",
+  "Temporal Distribution Score",
+  "Precipitation RMSE Score",
+  "SurfaceAirTemperature RMSE Score",
+  "Evapotranspiration RMSE Score",
+  "SurfaceDownwardSWRadiation RMSE Score",
+  "SurfaceNetSWRadiation RMSE Score"
+];
+
+const regionOptions = [
+  "Global - Land",
+  "Global - All",
+  "South America - Amazon"
+];
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  // const [scores, setScores] = useState(["Spatial Distribution Score"]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">CMEC</NavbarBrand>
+      </Navbar>
+      <div>
+        <ButtonGroup>
+          {scalarOptions.map((scalar, i) => (
+            <Button>{scalar}</Button>
+          ))}
+        </ButtonGroup>
+      </div>
+      <div>
+        <ButtonGroup>
+          {regionOptions.map((region, i) => (
+            <Button>{region}</Button>
+          ))}
+        </ButtonGroup>
+      </div>
     </div>
   );
 }

3ada24d7cd91357c8d22eb67cf58162f7f1e7cc7
William Hill <mjh2225@gmail.com>
2019-08-12 15:33:39 -0700

    added select box with search

--- src/Scalars.js
+++ src/Scalars.js
@@ -1,23 +1,27 @@
 import React, { Fragment, useRef, useEffect, useState } from "react";
+import SelectSearch from "react-select-search";
+import "./react_select_search.css";
 
 function Scalars(props) {
   console.log("props.scalars:", props);
+  let scalars = [];
+
+  for (let scalar of props.scalars) {
+    var lastIndex = scalar.lastIndexOf(" ");
+    let value = scalar.substring(0, lastIndex);
+    scalars.push({ name: scalar, value: value });
+  }
+  console.log("scalars:", scalars);
   return (
     <Fragment>
       <h2>Scalars</h2>
-      <div className="radio">
-        {props.scalars.map((scalar, i) => (
-          <label key={scalar} className="radio-inline scalarLabel">
-            <input
-              key={scalar}
-              type="radio"
-              className="radioLabel"
-              name="scalarRadio"
-              checked={props.scores === scalar}
-            />
-            {scalar}
-          </label>
-        ))}
+      <div className="text-center">
+        <SelectSearch
+          options={scalars}
+          value="Spatial Distribution"
+          name="scalars"
+          placeholder="Choose a scalar"
+        />
       </div>
     </Fragment>
   );

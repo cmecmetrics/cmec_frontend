69cf9e57d59357ef355c39ca235ef31bc84e1e92
William Hill <mjh2225@gmail.com>
2019-08-14 14:07:27 -0700

    added bulma styling

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,18 @@ import { Navbar, NavbarBrand } from "reactstrap";
 
 function Header() {
   return (
-    <Navbar color="light" light expand="md">
-      <NavbarBrand href="/">CMEC</NavbarBrand>
-    </Navbar>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://cmec.llnl.gov/">
+          <img src="cmec_logo.png" alt="CMEC Logo" width="112" height="28" /> <span>CMEC</span>
+        </a>
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
   );
 }
 

ba89d32217cd7428503292224a84ad51a7b2fc40
William Hill <mjh2225@gmail.com>
2020-02-15 11:25:46 -0800

    created constants.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ import { setGlobal, useGlobal } from "reactn";
 import Hyperslabs from "./Hyperslabs.js";
 import Models from "./Models.js";
 import Metrics from "./Metrics.js";
+import {
+  modelNames,
+  regionOptions,
+  metricOptions,
+  hyperslabOptions,
+  scalarOptions
+} from "./constants.js";
 
 // const CMEC_API_URL = "https://cmec-backend.herokuapp.com/api";
 const CMEC_API_URL = "http://localhost:5000/api";
@@ -20,14 +27,7 @@ const CMEC_API_URL = "http://localhost:5000/api";
 const width = 1000;
 const height = 600;
 const black = "#333333";
-const title = "My Data Visualization";
-const scalarColorScale = {
-  "Ecosystem and Carbon Cycle": "#ECFFE6",
-  "Hydrology Cycle": "#E6F9FF",
-  "Radiation and Energy Cycle": "#FFECE6",
-  Forcings: "#EDEDED",
-  Relationships: "#fff2e5"
-};
+const title = "CMEC Data Heatmap";
 
 function getBackgroundColor(rowName) {
   if (rowName.includes("Ecosystem and Carbon Cycle")) {
@@ -94,67 +94,6 @@ export const Visualization = styled.div`
   height: ${height}px;
 `;
 
-let metricOptions = [
-  "Ecosystem and Carbon Cycle",
-  "Hydrology Cycle",
-  "Radiation and Energy Cycle",
-  "Forcings",
-  "Relationships"
-];
-
-let modelNames = [
-  "bcc-csm1-1",
-  "bcc-csm1-1-m",
-  "CESM1-BGC",
-  "GFDL-ESM2G",
-  "inmcm4",
-  "IPSL-CM5A-LR",
-  "MIROC-ESM",
-  "MPI-ESM-LR",
-  "NorESM1-ME",
-  "MeanCMIP5",
-  "BCC-CSM2-MR",
-  "BCC-ESM1",
-  "CESM2",
-  "CESM2-WACCM",
-  "CNRM-CM6-1",
-  "CNRM-ESM2-1",
-  "E3SMv1-CTC",
-  "GISS-E2-1-G",
-  "GISS-E2-1-H",
-  "IPSL-CM6A-LR",
-  "MIROC6",
-  "MRI-ESM2-0",
-  "MeanCMIP6"
-];
-
-const scalarOptions = [
-  "Spatial Distribution Score",
-  "Bias Score",
-  "Overall Score",
-  "Seasonal Cycle Score",
-  "Interannual Variability Score",
-  "RMSE Score",
-  "Amplitude Score",
-  "Max Phase Score",
-  "Min Phase Score",
-  "Difference Score",
-  "Temporal Distribution Score",
-  "Precipitation RMSE Score",
-  "SurfaceAirTemperature RMSE Score",
-  "Evapotranspiration RMSE Score",
-  "SurfaceDownwardSWRadiation RMSE Score",
-  "SurfaceNetSWRadiation RMSE Score"
-];
-
-const regionOptions = {
-  "Global - Land": "global",
-  "Global - All": "globe",
-  "South America - Amazon": "southamericaamazon"
-};
-
-const hyperslabOptions = ["region", "metric", "scalar", "model"];
-
 setGlobal({
   scalar: "Overall Score",
   region: "global",
@@ -204,7 +143,6 @@ function App() {
     scalar: "Overall Score",
     model: ""
   });
-  const [filter, setFilter] = useState("ALL_SCORES");
   const [tableHeaderValues, setTableHeaderValues] = useGlobal(
     "tableHeaderValues"
   );
@@ -244,24 +182,20 @@ function App() {
   useEffect(() => {
     axios.post(`${CMEC_API_URL}/hyperslab`, apiParameters).then(response => {
       let rows;
-      let responseRegion;
       let data;
       if (!selectedRegion) {
         console.log("all regions selected");
       }
       console.log("selectedRegion:", selectedRegion);
-      if (filter === "ALL_SCORES") {
-        rows = Object.keys(response.data["RESULTS"][selectedRegion]);
-        data = response.data["RESULTS"][selectedRegion];
-      } else {
-        data = response.data[selectedRegion];
-        rows = Object.keys(responseRegion);
-      }
+      rows = Object.keys(response.data["RESULTS"][selectedRegion]);
+      data = response.data["RESULTS"][selectedRegion];
 
       let tableRows = rows.map((row, i) => {
+        console.log("row:", row);
         let [rowLevel, hierarchyLevel] = findHierarchyLevel(row);
         let [rowLabel, parent] = formatRowLabel(rowLevel, row);
         let columns = data[row][scalar];
+        console.log("columns:", columns);
 
         return (
           <TableRow
@@ -275,7 +209,6 @@ function App() {
             index={i}
             scalar={scalar}
             models={modelNames}
-            filter={filter}
           />
         );
       });
@@ -293,6 +226,16 @@ function App() {
             <Hyperslabs
               hyperslabOptions={hyperslabOptions}
               selectedHyperslab={selectedHyperslab}
+              title="X Axis Hyperslabs"
+            />
+          </div>
+        </div>
+        <div className="columns is-centered is-vcentered">
+          <div className="column">
+            <Hyperslabs
+              hyperslabOptions={hyperslabOptions}
+              selectedHyperslab={selectedHyperslab}
+              title="Y Axis Hyperslabs"
             />
           </div>
         </div>
